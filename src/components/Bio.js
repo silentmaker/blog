d72b004f39ba0343db944f57ed531156e0057c58
马兴骢 <demonmaxc@gmail.com>
2018-08-14 12:34:31 +0800

    update structure

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -4,27 +4,19 @@ import React from 'react'
 import 'typeface-montserrat'
 import 'typeface-merriweather'
 
-import profilePic from './profile.png'
+import profilePic from '../images/profile.png'
 import { rhythm } from '../utils/typography'
 
 class Bio extends React.Component {
   render() {
     return (
-      <div
-        style={{
-          display: 'flex'
-        }}
-      >
-        <img
-          src={profilePic}
-          alt={`Kyle Mathews`}
-          style={{
+      <div style={{ display: 'flex' }}>
+        <img src={profilePic} style={{
             marginRight: rhythm(1),
             marginBottom: 0,
             width: rhythm(2),
             height: rhythm(2),
-          }}
-        />
+          }} />
         <p>
           Written by <strong>Elvin Ma</strong> who lives and works in China building interesting things.{' '}
         </p>
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -10,22 +10,21 @@ class BlogPostTemplate extends React.Component {
   render() {
     const post = this.props.data.markdownRemark
     const siteTitle = get(this.props, 'data.site.siteMetadata.title')
+    const tags = (post.frontmatter.tags || []).map(tag => 
+      <Link key={tag} to={`/tags/${tag}`}>{tag}</Link>)
+    const categories = (post.frontmatter.categories || []).map(category => 
+      <Link key={category} to={`/categories/${category}`}>{category}</Link>)
     const { previous, next } = this.props.pathContext
 
     return (
       <div>
         <Helmet title={`${post.frontmatter.title} | ${siteTitle}`} />
         <h1>{post.frontmatter.title}</h1>
-        <p
-          style={{
-            ...scale(-1 / 5),
-            display: 'block',
-            marginBottom: rhythm(1),
-            marginTop: rhythm(-1),
-          }}
-        >
+        <div style={{ ...scale(-1) }}>
           {post.frontmatter.date}
-        </p>
+        </div>
+        {post.frontmatter.tags && <div>tags: {tags}</div>}
+        {post.frontmatter.categories && <div>categories: {categories}</div>}
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
         <hr
           style={{
@@ -80,6 +79,8 @@ export const pageQuery = graphql`
       html
       frontmatter {
         title
+        tags
+        categories
         date(formatString: "MMMM DD, YYYY")
       }
     }
