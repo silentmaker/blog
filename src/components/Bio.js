c95b3dd9b2092f53f14fe8c4d1ce78331524e688
马兴骢 <demonmaxc@gmail.com>
2018-08-15 01:05:45 +0800

    add projects block

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,25 +1,38 @@
 import React from 'react'
 
-// Import typefaces
 import 'typeface-montserrat'
 import 'typeface-merriweather'
 
-import profilePic from '../images/profile.png'
+import Projects from "./Projects"
+import profileImage from '../images/profile.png'
 import { rhythm } from '../utils/typography'
 
 class Bio extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isHideProjects: true }
+    this.showProjects = this.showProjects.bind(this)
+  }
+  showProjects() {
+    this.setState({ isHideProjects: !this.state.isHideProjects })
+  }
   render() {
     return (
       <div style={{ display: 'flex' }}>
-        <img src={profilePic} style={{
-            marginRight: rhythm(1),
+        <img src={profileImage} style={{
+            marginRight: rhythm(1/2),
             marginBottom: 0,
             width: rhythm(2),
             height: rhythm(2),
           }} />
-        <p>
-          Written by <strong>Elvin Ma</strong> who lives and works in China building interesting things.{' '}
-        </p>
+        <div style={{ flex: '1 1 0' }}>
+          <p>
+            Written by <strong>Elvin Ma</strong><br/>Living, working and building 
+            <span style={{ cursor: 'pointer', color: '#007acc' }} 
+              onClick={this.showProjects}> interesting stuff</span>.
+          </p>
+          <Projects hidden={this.state.isHideProjects} />
+        </div>
       </div>
     )
   }
---
+++ src/components/Projects.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import spriteImage from '../images/sprite.png'
+
+class Projects extends React.Component {
+  render() {
+    const { hidden } = this.props;
+    const projects = ['butler', 'tailor', 'earth', 'vision', 'todos', 
+      'wolvrine', 'lecturer', 'sprite-generator', 'calculator', 'gallery', 
+      'fiction', 'hero', 'icon-studio'];
+
+    return (
+      <ul style={{
+        display: 'flex',
+        visibility: hidden ? 'hidden' : 'visible',
+        opacity: hidden ? 0 : 1,
+        maxHeight: hidden ? 0 : 1000,
+        listStyle: 'none',
+        flexWrap: 'wrap',
+        transition: 'all 0.5s ease',
+        }}>
+        {projects.map((project, index) => 
+          <li key={project} style={{ flex: '0 1 120px' }}>
+            <a target="blank" href={`https://silentmaker.github.io/${project}`}>
+              <div style={{
+                width: 64,
+                height: 64,
+                margin: '0 auto',
+                backgroundRepeat: 'no-repeat',
+                backgroundImage: `url(${spriteImage})`,
+                backgroundSize: `auto 125px`,
+                backgroundPosition: `${-64 * index}px 0`,
+              }}></div>
+              <div style={{ color: '#999', textAlign: 'center' }}>
+                {project.charAt(0).toUpperCase() + project.slice(1)}
+              </div>
+            </a>
+          </li>
+        )}
+      </ul>
+    )
+  }
+}
+
+export default Projects
